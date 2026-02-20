9d6f98b99e2a894b19ce01b9d8d5b3b865706088
medihamad <kumarpatel692@gmail.com>
2026-02-20 23:32:22 +0300

    adding routes

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 
+//routes
 
+app.get('/', (req, res)=>{
+    res.send('welcome to express server');
+})
+
+app.get('/user', (req, res)=>{
+    res.send('user route');
+})
 
 app.listen(3000, ()=>{
     console.log('listenning on port ', 3000);
