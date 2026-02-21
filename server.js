3b474996096f244b7c00997c20d05f29ed757daa
medihamad <kumarpatel692@gmail.com>
2026-02-21 07:02:14 +0300

    adding middleware

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const express = require('express');
 
 const app = express();
 
-//routes
+//middlewares
+
+app.use(express.json());
+
+//routes path
 
 app.get('/', (req, res)=>{
     res.send('welcome to express server');
 })
 
+app.post('/', (req, res)=>{
+    console.log(req.body);
+    res.send('this is a post request');
+})
+
 app.get('/user', (req, res)=>{
     res.send('user route');
+    console.log(req.url, req.method)
+})
+
+app.get('/data', (req, res)=>{
+    res.status(200).json({
+        success: true,
+        message: "Data fetched"
+    })
 })
 
 app.listen(3000, ()=>{
